6bbe5bb7b002a0115a413862ba471265bcf6484d
Lisa Knox <lisaknox83@gmail.com>
2024-07-28 18:42:46 -0400

    added fullscreen to slide deck

--- src/components/SlideDeck.js
+++ src/components/SlideDeck.js
@@ -3,9 +3,11 @@ import Carousel from 'react-material-ui-carousel'
 import { Paper, Button } from '@mui/material'
 import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
 function SlideDeck(props)
 {
+    const handle = useFullScreenHandle();
     var items = [
         {
             name: "Random Name #1",
@@ -18,18 +20,23 @@ function SlideDeck(props)
     ]
 
     return (
-        <Carousel
-            autoPlay={false}
-            animation='slide'
-            indicators={false}
-            navButtonsAlwaysVisible={true}
-            NextIcon={<ArrowCircleRightIcon />}
-            PrevIcon={<ArrowCircleLeftIcon />}
-        >
-            {
-                items.map( (item, i) => <Item key={i} item={item} /> )
-            }
-        </Carousel>
+        <div>
+            <FullScreen handle={handle}>
+                <Carousel
+                    autoPlay={false}
+                    animation='slide'
+                    indicators={false}
+                    navButtonsAlwaysVisible={true}
+                    NextIcon={<ArrowCircleRightIcon />}
+                    PrevIcon={<ArrowCircleLeftIcon />}
+                >
+                    {
+                        items.map( (item, i) => <Item key={i} item={item} /> )
+                    }
+                </Carousel>
+            </FullScreen>
+            <button onClick={handle.enter}>Full Screen</button>
+        </div>
     )
 }
 
