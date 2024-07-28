fd87547eb3d13c44beb7e42359558fca2bdef780
Lisa Knox <lisaknox83@gmail.com>
2024-07-28 13:41:24 -0400

    jft makes axios call

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const cors = require('cors')
 const volleyball = require('volleyball')
 const app = express()
+const axios = require('axios')
 
 // static middleware
 app.use(express.static(path.join(__dirname, '..','public')))
@@ -11,7 +12,11 @@ app.use(cors())
 app.use(volleyball)
 
 //this is where some things should go
-
+app.get('/jft', async (req, res) => {
+  const jft = await axios.get('https://www.jftna.org/jft/')
+  console.log(jft)
+  res.json({jft: jft.data})
+})
 
 
 
--- src/views/JFT.js
+++ src/views/JFT.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+
+const fetchJFT = async () => {
+  const response = await axios.get("/jft");
+  console.log(response);
+};
 
 const JFT = () => {
+  useEffect(() => {
+    fetchJFT();
+  }, []);
+
   return (
     <div>
       <h1>JFT</h1>
