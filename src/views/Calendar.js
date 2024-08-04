3058af7f39a4d0b65ba1091b4a06cd81b3aea328
Lisa Knox <lisaknox83@gmail.com>
2024-08-04 15:10:13 -0400

    calendar css

--- src/views/Calendar.js
+++ src/views/Calendar.js
@@ -1,8 +1,17 @@
 import React from "react";
 
+const classes = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+};
+
 const Calendar = () => {
   return (
-    <div>
+    <div style={classes.container}>
       <iframe src="https://calendar.google.com/calendar/embed?src=0994f22fd2f97cedaa5213db3b2b8ab0f2325b0ec366356ec8aefc4dfd4b8f9f%40group.calendar.google.com&ctz=America%2FNew_York" style={{width:"80%", height:600 }} scrolling="no"></iframe>
     </div>
   );
