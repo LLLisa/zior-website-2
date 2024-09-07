49d376474327d04bcb37c5a902e583266508be6f
Lisa Knox <lisaknox83@gmail.com>
2024-09-07 13:19:04 -0400

    fixed width of fullscreen jft

--- src/views/JFT.js
+++ src/views/JFT.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import DOMPurify from 'dompurify';
 import { loadJFT } from '../store/jftReducer';
-import { FullScreen, useFullScreenHandle } from "react-full-screen";
+import { FullScreen, useFullScreenHandle } from 'react-full-screen';
 import theme from '../utils/theme';
 
 const classes = {
@@ -14,20 +14,41 @@ const classes = {
         marginBottom: '20px',
         fontSize: '16px',
     },
+    fullscreenJFT: {
+        fontSize: '24px',
+        backgroundColor: '#f9f9f9',
+        height: '100vh',
+        width: '80vw',
+        margin: '0 auto',
+        padding: '40px',
+        paddingTop: '10px',
+    },
 };
 
 const JFT = ({ jft, loadJFT }) => {
     const handle = useFullScreenHandle();
+    const [isFullScreen, setIsFullScreen] = useState(false);
 
     useEffect(() => {
-      if (!jft.length) loadJFT();
+        if (!jft.length) loadJFT();
     }, []);
+
+    const handleFullScreenChange = () => {
+        if (!document.fullscreenElement) {
+            setIsFullScreen(false);
+        } else {
+            setIsFullScreen(true);
+        }
+    };
 
     return (
         <div style={theme.contentContainer}>
-            <FullScreen handle={handle}>
+            <FullScreen
+                handle={handle}
+                onChange={handleFullScreenChange}
+            >
                 <div
-                    style={classes.renderedJft}
+                    style={isFullScreen ? classes.fullscreenJFT : classes.renderedJft}
                     dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(jft) }}
                 />
             </FullScreen>
