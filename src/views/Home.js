51ae8c369cf98629ba763508524d1b72aac5a2e5
Lisa Knox <lisaknox83@gmail.com>
2024-09-07 12:19:29 -0400

    fixed home page text

--- src/views/Home.js
+++ src/views/Home.js
@@ -24,7 +24,7 @@ const Home = () => {
             </div>
             <p>
                 We are an LGBTQIA+ meeting of Narcotics Anonymous, but as the Third Tradition states, the only requirement for membership is a desire to stop using - so ALL are
-                welcome here. We meet every Wednesday from 7:00 PM to 8:00 PM Eastern Time, which is {getEasternTimeAsLocal('19:00')} to {getEasternTimeAsLocal('20:00')} in your
+                welcome here. We meet every day from 7:00 PM to 8:00 PM Eastern Time, which is {getEasternTimeAsLocal('19:00')} to {getEasternTimeAsLocal('20:00')} in your
                 local time zone. You may join our meeting by clicking the link below or scanning the QR code on your phone.
             </p>
             <section style={classes.linksContainer}>
@@ -40,11 +40,11 @@ const Home = () => {
                     alt='Zoom QR code'
                 />
             </section>
-            <p>Also, pleasse joing us 15 minutes early for fellowship (we call it the 'parking lot') and to make sure you can get in. We look forward to seeing you there!</p>
+            <p>Also, please join us 15 minutes before the meeting for fellowship (we call it the 'parking lot') and to make sure you can get in. You can stay after the meeting to let us get to know you as well. We look forward to seeing you there!</p>
             <p>
-                The format of this meeting is a book study. We read the Just for Today daily meditation and then our speaker shares for 10-15 minutes on the reading. After that we
-                have open shares until 10 minutes before the hour, at which time we call for a burning desire. We have celebrate anniversaries on the last Wednesday of the month,
-                and our business meetings are held every 1st and 3rd Monday an hour before the meeting. check out <Link to='/calendar'>our calendar</Link> for more information.
+                The format of the meeting is a book study. We read the Just for Today daily meditation and then our speaker shares for 10-15 minutes on the reading. After that we
+                have open shares until 10 minutes before the end of the meeting, at which time we call for a burning desire. We have celebrate anniversaries on the last day of the month,
+                and our business meetings are held every 1st and 3rd Monday an hour before the meeting. Check out <Link to='/calendar'>our calendar</Link> for more information.
             </p>
             <p>We hope you will join us and find the love and support that we have found in this fellowship. And above all,</p>
             <p> KEEP COMING BACK!</p>
